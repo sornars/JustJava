a53cdf2d324f17f135afaa0b3faad7062de3bc35
Shivan Sornarajah <sornars@sornars.com>
2015-08-03 23:37:52 +0100

    Added localisations for Spanish

--- app/src/main/java/com/sornars/justjava/MainActivity.java
+++ app/src/main/java/com/sornars/justjava/MainActivity.java
@@ -16,7 +16,7 @@ import android.widget.Toast;
  */
 public class MainActivity extends ActionBarActivity {
 
-    int quantity = 2;
+    int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,20 +28,29 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        CheckBox topping1CheckBox = (CheckBox) findViewById(R.id.topping1_checkbox);
-        CheckBox topping2CheckBox = (CheckBox) findViewById(R.id.topping2_checkbox);
-        EditText nameInput = (EditText) findViewById(R.id.name_input);
-        Boolean topping1 = topping1CheckBox.isChecked();
-        Boolean topping2 = topping2CheckBox.isChecked();
-        String name = nameInput.getText().toString();
-        int price = calculatePrice(topping1, topping2);
-        String message = createOrderSummary(name, price, topping1, topping2);
-        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:"));
-        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "JustJava Order for " + name);
-        emailIntent.putExtra(Intent.EXTRA_TEXT, message);
-        if (emailIntent.resolveActivity(getPackageManager()) != null) {
-            startActivity(emailIntent);
+        if (quantity == 0) {
+            Context context = getApplicationContext();
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, getString(R.string.empty_order), duration);
+            toast.show();
+        } else {
+            CheckBox topping1CheckBox = (CheckBox) findViewById(R.id.topping1_checkbox);
+            CheckBox topping2CheckBox = (CheckBox) findViewById(R.id.topping2_checkbox);
+            EditText nameInput = (EditText) findViewById(R.id.name_input);
+            Boolean topping1 = topping1CheckBox.isChecked();
+            Boolean topping2 = topping2CheckBox.isChecked();
+            String name = nameInput.getText().toString();
+            int price = calculatePrice(topping1, topping2);
+            String message = createOrderSummary(name, price, topping1, topping2);
+            Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:"));
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject, name));
+            emailIntent.putExtra(Intent.EXTRA_TEXT, message);
+            if (emailIntent.resolveActivity(getPackageManager()) != null) {
+                startActivity(emailIntent);
+            }
         }
+
     }
 
     /**
@@ -63,7 +72,7 @@ public class MainActivity extends ActionBarActivity {
             Context context = getApplicationContext();
             int duration = Toast.LENGTH_SHORT;
 
-            Toast toast = Toast.makeText(context, "Cannot order more than 100 coffees!", duration);
+            Toast toast = Toast.makeText(context, getString(R.string.too_many_coffees), duration);
             toast.show();
         }
 
@@ -80,7 +89,7 @@ public class MainActivity extends ActionBarActivity {
             Context context = getApplicationContext();
             int duration = Toast.LENGTH_SHORT;
 
-            Toast toast = Toast.makeText(context, "Cannot order less than 1 coffee!", duration);
+            Toast toast = Toast.makeText(context, getString(R.string.too_few_coffees), duration);
             toast.show();
         }
 
@@ -96,11 +105,11 @@ public class MainActivity extends ActionBarActivity {
      */
     private int calculatePrice(boolean topping1, boolean topping2) {
         int price = 5;
-        if (topping1 == true) {
+        if (topping1) {
             price += 1;
         }
 
-        if (topping2 == true) {
+        if (topping2) {
             price += 2;
         }
 
@@ -117,12 +126,12 @@ public class MainActivity extends ActionBarActivity {
      * @return text summary
      */
     private String createOrderSummary(String name, int price, boolean topping1, boolean topping2) {
-        String orderSummaryMessage = "Name: " + name;
-        orderSummaryMessage += "\nHas Whipped Cream: " + topping1;
-        orderSummaryMessage += "\nHas Chocolate: " + topping2;
-        orderSummaryMessage += "\nQuantity: " + quantity;
-        orderSummaryMessage += "\nTotal $: " + price;
-        orderSummaryMessage += "\nThank you!";
+        String orderSummaryMessage = getString(R.string.name) + ": " + name;
+        orderSummaryMessage += "\n" + getString(R.string.topping1) + ": " + topping1;
+        orderSummaryMessage += "\n" + getString(R.string.topping2) + ": " + topping2;
+        orderSummaryMessage += "\n" + getString(R.string.quantity) + ": " + quantity;
+        orderSummaryMessage += "\n" + getString(R.string.total) + ": $" + price;
+        orderSummaryMessage += "\n" + getString(R.string.thanks);
         return orderSummaryMessage;
     }
 }
